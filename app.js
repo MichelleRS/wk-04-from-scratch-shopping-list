fa1bb132ad842e789ebef7fbc242b36e827fbcb3
Michelle Ross <miross629@gmail.com>
2022-11-29 09:17:32 -0800

    Begin event listener for form submit, including getting user input and error handling

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { createListItem } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const form = document.querySelector('#shopping-list-form');
+const error = document.querySelector('#error');
 
 /* State */
 
 /* Events */
+// listen for submit of form
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // form data instance to grab info we want from form
+    const data = new FormData(form);
+    // initialize variables for item and quantity using data.get to get user input
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    // new item
+    const newItem = await createListItem(item, quantity);
+
+    if (newItem) {
+        // display item
+        // display whole list (clear old list and fetch it again)
+    } else {
+        // error handling
+        error.textContent =
+            'Something went wrong with your input. Please check the item or quantity you entered and try again.';
+    }
+});
 
 /* Display Functions */
+// loop, render, append
+// render differently if purchased is true
